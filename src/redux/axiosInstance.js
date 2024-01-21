ac5a919f4cb36124397a0c190c80920b916e8c07
Irson <jiordesta@gmail.com>
2024-01-21 20:47:50 +0800

    ready to deploy

--- src/redux/axiosInstance.js
+++ src/redux/axiosInstance.js
@@ -4,7 +4,7 @@ export const axiosInstance = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
       ? "http://localhost:8800/todolist"
-      : "",
+      : "https://todolist-webapp-api.onrender.com/todolist",
   timeout: 50000,
   withCredentials: true,
 });
